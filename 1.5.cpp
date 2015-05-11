b0b5fe6f4f49d9a71c4a0d62978c551c89161bf4
AAAyag <491237847@qq.com>
2015-05-11 09:39:41 +0800

    first commit

---
+++ 1.5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string usr_name;
+	cout << "please input your name: ";
+	cin >> usr_name;
+
+	switch (usr_name.size()){
+		case 0:
+			cout << "Ah, the usr with no name."
+				<< "well, ok, hi, usr with no name \n";
+			break;
+		case 1:
+			cout << "Ah, 1-character name?"
+				<< "hello, " << usr_name << endl;
+			break;
+		default:
+			cout << "hello, " << usr_name
+				<< "-- happy to make your acquaintance! \n";
+			break;
+	}
+	return 0;
+}
