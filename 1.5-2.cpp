7184854d9e1b8f9fcf694e4542160e6d2e0f687d
AAAyag <491237847@qq.com>
2015-05-11 11:36:47 +0800

    add 1.5-2.cpp, c-styl

---
+++ 1.5-2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+	//allocate a fixed size memery
+	const int num_size = 128;
+	char user_name[num_size];
+	const char* ptr_name = "zhangxuewu";
+	cout << "Please input your name:";
+	cin >> setw(num_size) >> user_name;
+	switch (strlen(user_name))
+	{
+		case 127:
+			cout << ">127 character \n";
+		default:
+			cout << "hello: " << user_name <<endl;
+            cout << "my name: " << ptr_name <<endl;
+			break;
+	}
+}
