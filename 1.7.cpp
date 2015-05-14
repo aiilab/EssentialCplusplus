3db1c0a07e7cf3d9d9ad0df5330f3dc103132890
AAAyag <491237847@qq.com>
2015-05-14 11:07:08 +0800

    add fstream ifstream ofstream operation

--- 1.7.cpp
+++ 1.7.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int main()
 {
+	// read file 
+	cout << " read files \n";
 	ifstream in_file("ifile.txt");
 	if (!in_file)
 	{	cerr << "oops! unable to open input file"; return -1; }
@@ -18,6 +20,21 @@ int main()
 		cout << " the name is :" << name << endl;
 		in_file >> gender >> age;
 		cout << "the gender: " << gender << "; the ag :" << age << endl; 
+	}
+
+	// write file
+	cout << "write files \n";
+	ofstream out_files("ofiles.txt");
+	if (!out_files)
+	{ cerr << "oops, unable to open output files"; return -1; }
+	int cnt = 3;
+	while (cnt--)
+	{
+		cout << "name";
+		cin >> name;
+		cout << "age:";
+		cin >> age;
+		out_files << name << age << endl;
 	}
 	return 0;
 }
