818e0a42e463e23ecbc33e799d09f526874ca96a
AAAyag <491237847@qq.com>
2015-05-15 09:36:37 +0800

    add 2.2.cpp ostream = cout

---
+++ 2.2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool calc_elements(vector<int> &vec, int pos);
+void display_elems(vector<int> &vec, const string &title, ostream &os=cout);
+int main ()
+{
+	vector<int> pent;
+	const string title ("Pentagonal Numeric Series");
+
+	if (calc_elements (pent, 0))
+		display_elems(pent, title);
+
+	if (calc_elements (pent, 8))
+		display_elems(pent, title);
+
+
+	if (calc_elements (pent, 14))
+		display_elems(pent, title);
+
+	if (calc_elements (pent, 138))
+		display_elems(pent, title);
+}
+
+bool calc_elements (vector<int> &vec, int pos)
+{
+	if (pos <=0 || pos >64)
+	{
+		cerr << "sorry. Invalid position:" << pos << endl;
+		return false;
+	}		
+	for (int ix = vec.size()+1; ix <= pos; ++ix)
+		vec.push_back( (ix*(3*ix-1))/2 );
+	return true;
+}
+void display_elems (vector<int> &vec, const string &title, ostream &os)
+{
+	os <<'\n' << title << "\n\t";
+	for (int  ix = 0; ix < vec.size(); ++ix)
+		os << vec[ix] << ' ';
+	os << endl;
+}
